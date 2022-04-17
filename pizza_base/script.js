715cc9fb0e53294b2864b1cd004bef2f6a1909b4
Rafael <rafaeldovale666@gmail.com>
2022-04-17 07:07:50 -0300

    inserindo o modal e preenchendo as informações

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -13,6 +13,7 @@ pizzaJson.map(function(item, index){
     //console.log(item)
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
 
+    pizzaItem.setAttribute('data-key', index);
     //inserindo a imagem
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     //preencher o valor da unidade
@@ -21,6 +22,27 @@ pizzaJson.map(function(item, index){
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     //preencher as descrições
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    
+
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        //console.log(pizzaJson[key]);
+        //console.log('Piza clicada:' +key);
+
+        //começando a preencher as informações no modal
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
+
+        //console.log('clicou na pizza')
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(function(){
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
     c('.pizza-area').append( pizzaItem )
-})
+});
+
+
